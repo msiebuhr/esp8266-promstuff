dd31f043be434df0994fb9f51032bb1104d50abc
Morten Siebuhr <sbhr@sbhr.dk>
2017-10-05 21:12:14 +0200

    Release v1.0.5

--- esp8266-thermometer-ds18b20.ino
+++ esp8266-thermometer-ds18b20.ino
@@ -9,7 +9,7 @@
 #include <SPIFFSEditor.h>
 
 #define APPNAME "prom_ds18b20_sensor"
-#define VERSION "V1.0.4"
+#define VERSION "V1.0.5"
 #define COMPDATE __DATE__ __TIME__
 #define MODEBUTTON D3
 IOTAppStory IAS(APPNAME, VERSION, COMPDATE, MODEBUTTON);
